046ec9432e574e435667c4980c7c34d1629754cf
Thiago Izidoro <thiagomachadox@gmail.com>
2021-11-10 11:00:37 -0300

    feat: list all users usecase and controller

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -1,15 +1,13 @@
 import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
-interface IRequest {
-  user_id: string;
-}
-
 class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+  execute(): User[] {
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
